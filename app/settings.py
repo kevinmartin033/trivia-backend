a45f613177930ab0721339f33f2bc4faeee94f7c
Kevin Martin <kevinmartin33@gmil.com>
2021-07-24 23:19:28 +0100

    Add STATIC_ROOT setting

--- app/settings.py
+++ app/settings.py
@@ -19,7 +19,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,8 +123,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.realpath(__file__), '/static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
